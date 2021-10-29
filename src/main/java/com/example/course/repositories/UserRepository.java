02e41eef0e949206b130c485bde9c537159b3c31
Victor Hoffmann <viictorhoffmann@gmail.com>
2021-10-29 08:15:45 -0300

    Component registration

--- src/main/java/com/example/course/repositories/UserRepository.java
+++ src/main/java/com/example/course/repositories/UserRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-	
-	
+		
 }
